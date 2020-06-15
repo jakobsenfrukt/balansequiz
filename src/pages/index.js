36952969a748594fd50fc5173667eb682b787ff2
elisejakob <hei@elisejakob.no>
2020-06-15 12:48:54 +0200

    design progress, chapter numbering and quotes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,19 @@ const IndexPage = () => (
     <SEO title="Start" />
     <section className="green">
       <h1>Velkommen!</h1>
-      <label>Hvilken organisasjon tilhører du?</label>
-      <input type="text"></input>
+      <label for="org">
+        Hvilken organisasjon tilhører du?
+        <input id="org" name="org" type="text" aria-label="Hvilken organisasjon tilhører du?" placeholder="Navn på organisasjon"></input>
+      </label>
       <p>Vi lagrer informasjonen anonymt. Vi spør fordi vi trenger det til rapporter til Kulturrådet blablabla.</p>
     </section>
     <section className="pink">
-      <h1>Yo</h1>
+      <h1>
+        <span class="chapter-no">Del 1</span> En overskrift til en del av kurset
+      </h1>
     </section>
     <section>
+      <header class="chapter-header">Del 1: Seksuell trakassering i tall</header>
       <p>Tall fra Statistisk Sentralbyrå viser at</p>
       <h1>4 %</h1>
       <p>av sysselsatte i Norge opplever uønsket seksuell oppmerksomhet, kommentarer eller lignende, et par ganger i måneden eller oftere.</p>
@@ -37,6 +42,10 @@ const IndexPage = () => (
     </section>
     <section className="darkgreen">
       <h1>Yo</h1>
+    </section>
+    <section>
+      <blockquote>Hvorfor sa du ikke bare nei?</blockquote>
+      <p>Mange som forteller om opplevelser med seksuell trakassering og overgrep blir møtt med spørsmål om hva de selv gjorde for å sette grenser.</p>
     </section>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
