7313610e7a3a42a5fa15f26564eaf801ab8cac09
elisejakob <hei@elisejakob.no>
2020-08-15 22:24:23 +0200

    start risk styling

--- src/components/risk.js
+++ src/components/risk.js
@@ -2,7 +2,14 @@ import React from "react"
 
 const Risks = ({ siteTitle }) => (
   <section className="slide">
-    <h2>Risiko!!!</h2>
+    <h2 className="risk-heading">Oppdragsarbeid</h2>
+    <p className="risk-description">I kulturlivet jobber mange frilans og som selvstendig næringsdrivende. Uten fast ansettelse kan det oppleves vanskelig å varsle i frykt for at konflikter kan ødelegge for fremtidige karrieremuligheter. Det kan skape en problematisk maktbalanse at frilansere er mer utskiftbare enn faste ansatte.</p>
+    <div className="risk-options">
+      <div className="risk-option-button">Ingen risiko</div>
+      <div className="risk-option-button">Lav risiko</div>
+      <div className="risk-option-button">Middels risiko</div>
+      <div className="risk-option-button">Høy risiko</div>
+    </div>
   </section>
 )
 
