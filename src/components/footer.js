86abba8304a69b4612ef5159acc4a9d9ac2f1e18
elisejakob <hei@elisejakob.no>
2020-08-17 21:38:38 +0200

    style site footer and chapter header + minor quiz adjustments

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,12 +4,10 @@ import Logo from "../images/BKlogo.svg"
 
 const Footer = () => (
   <footer className="site-footer">
-    <div>
-      Et initiativ fra
-      <Link to="https://balansekunstprosjektet.no" target="_blank">
-        <img className="logo" src={Logo} alt="Logo for Balansekunstprosjektet" />
-      </Link>
-    </div>
+    <Link to="https://balansekunstprosjektet.no" target="_blank">
+      <span>Et initiativ fra</span><br />
+      <img className="logo" src={Logo} alt="Logo for Balansekunstprosjektet" />
+    </Link>
   </footer>
 )
 
