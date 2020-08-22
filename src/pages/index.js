e42684462430380e7d61b908a816d6998fe8638f
elisejakob <hei@elisejakob.no>
2020-08-22 11:33:49 +0200

    remove sketches from front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,31 +31,6 @@ const IndexPage = () => (
         </p>
       </div>
     </section>
-    <section className="slide">
-      <h1>Skisser</h1>
-      <p>Under følger skisser til de interaktive komponentene. Disse er ikke funksjonelle, så gå til <Link to="/chapters">Liste over kapitler</Link> om du vil teste :)</p>
-    </section>
-    <section className="slide">
-      <h1>På linje</h1>
-      <p>I denne oppgaven skal du ta stilling til ulike påstander om seksuell trakassering. Oppgi tall fra 1 (helt uenig) til 10 (helt enig) for å uttrykke hva du mener om påstandene.</p>
-    </section>
-    <section className="slide">
-      <h2 className="statement-heading">Hvor enig er du?</h2>
-      <p className="statement">Seksuell trakassering er mer utbredt i kulturlivet enn i andre sektorer</p>
-      <div className="poll-options">
-        <div className="poll-option-label">Uenig</div>
-        <div className="poll-option-buttons">
-          <div className="poll-option-button">1</div>
-          <div className="poll-option-button">2</div>
-          <div className="poll-option-button">3</div>
-          <div className="poll-option-button">4</div>
-          <div className="poll-option-button">5</div>
-          <div className="poll-option-button">6</div>
-          <div className="poll-option-button">7</div>
-        </div>
-        <div className="poll-option-label">Enig</div>
-      </div>
-    </section>
   </Layout>
 )
 
