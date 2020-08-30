e53bc00fd7a00dc39560566e0241182dadab0b9f
Jonas Braathen <jonas@ponas.no>
2020-08-30 23:31:23 +0200

    Don't use Link for external links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,13 @@
-import { Link } from "gatsby"
 import React from "react"
 import Logo from "../images/BKlogo.svg"
 
 const Footer = () => (
   <footer className="site-footer">
-    <Link to="https://balansekunstprosjektet.no" target="_blank">
-      <span>Et initiativ fra</span><br />
+    <a href="https://balansekunstprosjektet.no" target="_blank">
+      <span>Et initiativ fra</span>
+      <br />
       <img className="logo" src={Logo} alt="Logo for Balansekunstprosjektet" />
-    </Link>
+    </a>
   </footer>
 )
 
