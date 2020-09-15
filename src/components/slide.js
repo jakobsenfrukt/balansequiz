32a66086cf9fe0f079537450fb6ed9efff6a1cae
Jonas Braathen <jonas@ponas.no>
2020-09-15 20:34:26 +0200

    Simplify bgIsWhite logic

--- src/components/slide.js
+++ src/components/slide.js
@@ -9,14 +9,8 @@ const Slide = ({ data, index }) => {
   const slideStyle = { backgroundColor: backgroundColorHex }
   const darkColors = ["#e269a3", "#0f4138"]
   const bgIsDark = darkColors.includes(backgroundColorHex.toLowerCase())
-  const bgIsWhite = function() {
-    if (backgroundColor) {
-      return backgroundColorHex === "#ffffff"
-    } else {
-      return true
-    }
-  }
-  const extraClasses = `${bgIsDark ? "invert" : ""} ${bgIsWhite() ? "" : "bgcolor"}`
+  const bgIsWhite = !backgroundColor || backgroundColorHex === "#ffffff"
+  const extraClasses = `${bgIsDark ? "invert" : ""} ${bgIsWhite ? "" : "bgcolor"}`
 
   if (type === "Balanse_SlideKapittelforside") {
     const { images, chapterTitle: title } = data
